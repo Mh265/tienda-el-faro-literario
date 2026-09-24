c8c19b96dd44fe5a624e472f4d10e5d15ea3a6ab
hermanitoslj123-dot <hermanitoslj123@gmail.com>
2026-09-24 00:01:19 -0600

    desarrollo modulo header.php

---
+++ includes/plantillas/header.php
@@ -0,0 +1,9 @@
+<!-- includes/plantillas/header.php -->
+<a class="navbar-brand d-flex align-items-center gap-2"
+   href="<?= $rutaBase ?>public/index.php">
+  <img src="<?= $rutaBase ?>assets/img/logo.png"
+       alt="Logo de El Faro Literario"
+       class="logo-marca"
+       height="40">
+  <span>El Faro Literario</span>
+</a>
